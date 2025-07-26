98ce1e19b09321bbd3419c87d1da29cd785480d3
Vlad <vlad.didkovskiy.it@gmail.com>
2025-07-26 16:41:29 +0300

    Wo4

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,40 +1,10 @@
 import { getLocalStorage, loadHeaderFooter } from "./utils.mjs";
+import ShoppingCart from "./ShoppingCart.mjs";
 
 loadHeaderFooter();
 
-function renderCartContents() {
-    const cartItems = getLocalStorage("so-cart") || [];
-    const productList = document.querySelector(".product-list");
-    const footer = document.querySelector(".list-footer");
-    const totalDisplay = document.querySelector(".list-total");
-
-    if (cartItems.length > 0) {
-
-        const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-        productList.innerHTML = htmlItems.join("");
-
-        footer.classList.remove("hide");
-
-        const total = cartItems.reduce((sum, item) => sum + item.FinalPrice, 0);
-        totalDisplay.textContent = `Total: $${total.toFixed(2)}`;
-    } else {
-        productList.innerHTML = "<p>Your cart is empty.</p>";
-        footer.classList.add("hide");
-    }
+const cart = new ShoppingCart("so-cart", ".product-list");
+cart.init();
+if (cart.total > 0) {
+    document.querySelector(".list-footer").classList.remove("hide");
 }
-
-function cartItemTemplate(item) {
-    return `<li class="cart-card divider">
-    <a href="#" class="cart-card__image">
-      <img src="${item.Image}" alt="${item.Name}" />
-    </a>
-    <a href="#">
-      <h2 class="card__name">${item.Name}</h2>
-    </a>
-    <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-    <p class="cart-card__quantity">qty: 1</p>
-    <p class="cart-card__price">$${item.FinalPrice}</p>
-  </li>`;
-}
-
-renderCartContents();
